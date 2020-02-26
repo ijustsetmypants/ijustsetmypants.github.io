ea0a4d9aa451d1e65d7e68eb0d7d9d5e1fd206f2
Brandon Shepard <brandon.shepard6@icloud.com>
2020-02-25 21:52:56 -0600

    Updates

---
+++ precache-manifest.28ea31f0f2cd4bcefcafec8738e35f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9466361b572943ab14f0a933c6eb3443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9216fd79375e5a1701e1",
+    "url": "/static/css/main.46c0f7c7.chunk.css"
+  },
+  {
+    "revision": "3cb71e6948f2e4853758",
+    "url": "/static/js/2.43eb05a8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.43eb05a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9216fd79375e5a1701e1",
+    "url": "/static/js/main.1bde0b4a.chunk.js"
+  },
+  {
+    "revision": "ca1aebe378b9d21fc748",
+    "url": "/static/js/runtime-main.1ebbc306.js"
+  }
+]);
